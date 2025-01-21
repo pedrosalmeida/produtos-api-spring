5e3487ada092a9a4cc62529c1936547931a15b1b
PAMPACOMPASSO\pedro.palmieri <pedro.palmieri@compasso.com.br>
2025-01-21 17:03:16 -0300

    Projeto de treinamento Spring Boot

---
+++ src/main/java/com/example/produtosapi/model/Produto.java
@@ -0,0 +1,66 @@
+package com.example.produtosapi.model;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+// POJO -> Plain Old Java Object
+
+@Entity
+@Table(name = "produto")
+public class Produto {
+    @Id
+    @Column(name = "id")
+    private String id;
+
+    @Column(name = "nome")
+    private String nome;
+
+    @Column(name = "descricao")
+    private String descricao;
+
+    @Column(name = "preco")
+    private Double preco;
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    @Override
+    public String toString() {
+        return "Produto{" +
+                "id='" + id + '\'' +
+                ", nome='" + nome + '\'' +
+                ", descricao='" + descricao + '\'' +
+                ", preco=" + preco +
+                '}';
+    }
+}
